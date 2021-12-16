f05e5aba92e2efd095d98d4292e63c642cfb5690
Kravchuk Anton <Kravch-anton@mail.ru>
2021-12-16 22:55:33 +0300

    first

---
+++ dist/main.js
@@ -0,0 +1,108 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/style.css":
+/*!***********************!*\
+  !*** ./src/style.css ***!
+  \***********************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\\nHookWebpackError: Module build failed (from ./node_modules/css-loader/dist/cjs.js):\\nCssSyntaxError\\n\\n(2:7) E:\\\\shop\\\\funiro\\\\src\\\\style.css Unknown word\\n\\n \\u001b[90m 1 | \\u001b[39m\\n\\u001b[1m\\u001b[31m>\\u001b[39m\\u001b[22m\\u001b[90m 2 | \\u001b[39m      import API from \\u001b[32m\\\"!../node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js\\\"\\u001b[39m\\u001b[33m;\\u001b[39m\\n \\u001b[90m   | \\u001b[39m      \\u001b[1m\\u001b[31m^\\u001b[39m\\u001b[22m\\n \\u001b[90m 3 | \\u001b[39m      import domAPI from \\u001b[32m\\\"!../node_modules/style-loader/dist/runtime/styleDomAPI.js\\\"\\u001b[39m\\u001b[33m;\\u001b[39m\\n \\u001b[90m 4 | \\u001b[39m      import insertFn from \\u001b[32m\\\"!../node_modules/style-loader/dist/runtime/insertBySelector.js\\\"\\u001b[39m\\u001b[33m;\\u001b[39m\\n\\n    at tryRunOrWebpackError (E:\\\\shop\\\\funiro\\\\node_modules\\\\webpack\\\\lib\\\\HookWebpackError.js:88:9)\\n    at __webpack_require_module__ (E:\\\\shop\\\\funiro\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:4979:12)\\n    at __webpack_require__ (E:\\\\shop\\\\funiro\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:4936:18)\\n    at E:\\\\shop\\\\funiro\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5007:20\\n    at symbolIterator (E:\\\\shop\\\\funiro\\\\node_modules\\\\neo-async\\\\async.js:3485:9)\\n    at done (E:\\\\shop\\\\funiro\\\\node_modules\\\\neo-async\\\\async.js:3527:9)\\n    at Hook.eval [as callAsync] (eval at create (E:\\\\shop\\\\funiro\\\\node_modules\\\\tapable\\\\lib\\\\HookCodeFactory.js:33:10), <anonymous>:15:1)\\n    at Hook.CALL_ASYNC_DELEGATE [as _callAsync] (E:\\\\shop\\\\funiro\\\\node_modules\\\\tapable\\\\lib\\\\Hook.js:18:14)\\n    at E:\\\\shop\\\\funiro\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:4914:43\\n    at symbolIterator (E:\\\\shop\\\\funiro\\\\node_modules\\\\neo-async\\\\async.js:3482:9)\\n-- inner error --\\nError: Module build failed (from ./node_modules/css-loader/dist/cjs.js):\\nCssSyntaxError\\n\\n(2:7) E:\\\\shop\\\\funiro\\\\src\\\\style.css Unknown word\\n\\n \\u001b[90m 1 | \\u001b[39m\\n\\u001b[1m\\u001b[31m>\\u001b[39m\\u001b[22m\\u001b[90m 2 | \\u001b[39m      import API from \\u001b[32m\\\"!../node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js\\\"\\u001b[39m\\u001b[33m;\\u001b[39m\\n \\u001b[90m   | \\u001b[39m      \\u001b[1m\\u001b[31m^\\u001b[39m\\u001b[22m\\n \\u001b[90m 3 | \\u001b[39m      import domAPI from \\u001b[32m\\\"!../node_modules/style-loader/dist/runtime/styleDomAPI.js\\\"\\u001b[39m\\u001b[33m;\\u001b[39m\\n \\u001b[90m 4 | \\u001b[39m      import insertFn from \\u001b[32m\\\"!../node_modules/style-loader/dist/runtime/insertBySelector.js\\\"\\u001b[39m\\u001b[33m;\\u001b[39m\\n\\n    at Object.<anonymous> (E:\\\\shop\\\\funiro\\\\node_modules\\\\css-loader\\\\dist\\\\cjs.js!E:\\\\shop\\\\funiro\\\\node_modules\\\\style-loader\\\\dist\\\\cjs.js!E:\\\\shop\\\\funiro\\\\node_modules\\\\sass-loader\\\\dist\\\\cjs.js!E:\\\\shop\\\\funiro\\\\node_modules\\\\postcss-loader\\\\dist\\\\cjs.js??ruleSet[1].rules[0].use[4]!E:\\\\shop\\\\funiro\\\\src\\\\style.css:1:7)\\n    at E:\\\\shop\\\\funiro\\\\node_modules\\\\webpack\\\\lib\\\\javascript\\\\JavascriptModulesPlugin.js:432:11\\n    at Hook.eval [as call] (eval at create (E:\\\\shop\\\\funiro\\\\node_modules\\\\tapable\\\\lib\\\\HookCodeFactory.js:19:10), <anonymous>:7:1)\\n    at Hook.CALL_DELEGATE [as _call] (E:\\\\shop\\\\funiro\\\\node_modules\\\\tapable\\\\lib\\\\Hook.js:14:14)\\n    at E:\\\\shop\\\\funiro\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:4981:39\\n    at tryRunOrWebpackError (E:\\\\shop\\\\funiro\\\\node_modules\\\\webpack\\\\lib\\\\HookWebpackError.js:83:7)\\n    at __webpack_require_module__ (E:\\\\shop\\\\funiro\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:4979:12)\\n    at __webpack_require__ (E:\\\\shop\\\\funiro\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:4936:18)\\n    at E:\\\\shop\\\\funiro\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5007:20\\n    at symbolIterator (E:\\\\shop\\\\funiro\\\\node_modules\\\\neo-async\\\\async.js:3485:9)\\n\\nGenerated code for E:\\\\shop\\\\funiro\\\\node_modules\\\\css-loader\\\\dist\\\\cjs.js!E:\\\\shop\\\\funiro\\\\node_modules\\\\style-loader\\\\dist\\\\cjs.js!E:\\\\shop\\\\funiro\\\\node_modules\\\\sass-loader\\\\dist\\\\cjs.js!E:\\\\shop\\\\funiro\\\\node_modules\\\\postcss-loader\\\\dist\\\\cjs.js??ruleSet[1].rules[0].use[4]!E:\\\\shop\\\\funiro\\\\src\\\\style.css\\n1 | throw new Error(\\\"Module build failed (from ./node_modules/css-loader/dist/cjs.js):\\\\nCssSyntaxError\\\\n\\\\n(2:7) E:\\\\\\\\shop\\\\\\\\funiro\\\\\\\\src\\\\\\\\style.css Unknown word\\\\n\\\\n \\\\u001b[90m 1 | \\\\u001b[39m\\\\n\\\\u001b[1m\\\\u001b[31m>\\\\u001b[39m\\\\u001b[22m\\\\u001b[90m 2 | \\\\u001b[39m      import API from \\\\u001b[32m\\\\\\\"!../node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js\\\\\\\"\\\\u001b[39m\\\\u001b[33m;\\\\u001b[39m\\\\n \\\\u001b[90m   | \\\\u001b[39m      \\\\u001b[1m\\\\u001b[31m^\\\\u001b[39m\\\\u001b[22m\\\\n \\\\u001b[90m 3 | \\\\u001b[39m      import domAPI from \\\\u001b[32m\\\\\\\"!../node_modules/style-loader/dist/runtime/styleDomAPI.js\\\\\\\"\\\\u001b[39m\\\\u001b[33m;\\\\u001b[39m\\\\n \\\\u001b[90m 4 | \\\\u001b[39m      import insertFn from \\\\u001b[32m\\\\\\\"!../node_modules/style-loader/dist/runtime/insertBySelector.js\\\\\\\"\\\\u001b[39m\\\\u001b[33m;\\\\u001b[39m\\\\n\\\");\");\n\n//# sourceURL=webpack://funiro/./src/style.css?");
+
+/***/ }),
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.css */ \"./src/style.css\");\n/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_style_css__WEBPACK_IMPORTED_MODULE_0__);\n\r\n\r\nconst userStack = {\r\n    language: 'JavaScript',\r\n}\r\n\r\nconst user = {\r\n    name: Anton,\r\n    age: 26,\r\n    ...userStack\r\n}\r\n\r\nconsole.log(user)\n\n//# sourceURL=webpack://funiro/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
+/******/ 	
+/******/ })()
+;
